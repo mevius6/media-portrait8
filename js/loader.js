665d037aada423afff3e03dd000c019dd41fb302
Nicolai <iplexus@icloud.com>
2019-05-16 16:02:49 +0300

    Removed jQuery

--- js/loader.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$("body").addClass("page__loading")
-})
-$(window).on("load", function() {
-	$(".page__loader-wrap").fadeOut("slow", function() {
-		$("body").removeClass("page__loading")
-	})
-})
